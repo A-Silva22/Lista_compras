23075651c8771f78475039f26b92de3911123485
Asilva <alexandra.silva2297@gmail.com>
2026-03-06 17:11:34 +0000

    Add EMAIL_USE_SSL support for port 465 SMTP

--- lista_compras/settings.py
+++ lista_compras/settings.py
@@ -155,7 +155,8 @@ EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='ListaIsto <geral@listaisto.pt>')
 
 AUTHENTICATION_BACKENDS = [
